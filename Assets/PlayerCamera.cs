99c5ca4d99c58682dd3797e755ae34cc5ae727bf
Yuriy_Garev <yuriy.garev@gmail.com>
2019-08-19 01:38:02 +0200

    Platformer parameters; Scene changes; Camera script

---
+++ Assets/PlayerCamera.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed*Time.deltaTime);
+        transform.position = smoothedPosition;
+        
+    }
+}
