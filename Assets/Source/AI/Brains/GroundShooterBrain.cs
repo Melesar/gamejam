f94a8ade9de25baf72c012647f3fc126ba5ed431
Melesar <sergey.pikhota@gmail.com>
2019-08-24 23:09:08 +0200

    Moved scripts to separate files

--- Assets/Source/AI/Brains/GroundShooterBrain.cs
+++ Assets/Source/AI/Brains/GroundShooterBrain.cs
@@ -2,24 +2,6 @@ using UnityEngine;
 
 namespace Source.AI
 {
-    [CreateAssetMenu(menuName = "Brains/Ground shooter")]
-    public class GroundShooterBrainAsset : AIBrainAsset
-    {
-        [SerializeField] private float _maxTravelDistance;
-        [SerializeField] private LayerMask _noPlayerMask;
-        [SerializeField] private float _attackRange;
-        
-        public override AIBrain GetBrain(AIContext context, AIControllersRepository controllersRepository)
-        {
-            return new GroundShooterBrain(context, controllersRepository, new GroundShooterBrain.BrainParams
-            {
-                MaxTravelDistance = _maxTravelDistance,
-                NoPlayerMask = _noPlayerMask,
-                AttackRange = _attackRange,
-            });
-        }
-    }
-
     public class GroundShooterBrain : AIBrain
     {
         public class BrainParams
