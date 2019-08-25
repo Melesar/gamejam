555de1516833ca406b125223c675da9085e3b76c
Melesar <sergey.pikhota@gmail.com>
2019-08-25 12:03:13 +0200

    Shooting direction

--- Assets/Source/PlayerAttack/PlatformerAttackController.cs
+++ Assets/Source/PlayerAttack/PlatformerAttackController.cs
@@ -9,11 +9,16 @@ namespace Source.PlayerAttack
     {
         protected override Vector3 GetShootDirection()
         {
+            var camera = Camera.main;
+            var shootingOrigin = Refs.shootingOrigin.position;
+            var direction = shootingOrigin - camera.transform.position;
+            direction = Vector3.Project(direction, camera.transform.forward);
+            
             var mousePosition = Input.mousePosition;
-            mousePosition.z = 10;
-            var mousePos = Camera.main.ScreenToWorldPoint(mousePosition);
-            var direction = mousePos - Refs.shootingOrigin.position;
-            direction = Vector3.ProjectOnPlane(direction.normalized, Vector3.up);
+            mousePosition.z = direction.magnitude;
+            var mousePos = camera.ScreenToWorldPoint(mousePosition);
+            direction = mousePos - shootingOrigin;
+            direction = Vector3.ProjectOnPlane(direction, Vector3.up).normalized;
             
             return direction;
         }
